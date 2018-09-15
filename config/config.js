24e297a004eb7e664c43a37672f17559022fff4a
dinesh.kumawat <dinesh.kumawat@guidingstardigital.com>
2018-09-15 21:51:13 +0530

    changes done

--- config/config.js
+++ config/config.js
@@ -3,5 +3,5 @@ const localdb = 'mongodb://localhost:27017/TodoApp';
 const devdb = 'mongodb://dinesh:todoapptestpassword1@ds257732.mlab.com:57732/todoapptest';
 if(env==='development'){
   process.env.PORT=3000;
-  process.env.MONGODB_URI = devdb;
+  process.env.MONGODB_URI = localdb;
 }
--- server/server.js
+++ server/server.js
@@ -88,6 +88,16 @@ app.patch('/todos/:id',(req,res)=>{
   });
 });
 
+app.post('/users',(req,res)=>{
+  var body = _.pick(req.body,['email','password']);
+  var user = new User(body);
+  user.save().then((user)=>{
+    res.send(user);
+  }).catch((error)=>{
+    res.status(400).send(error); 
+  });
+});
+
 app.listen(port,()=>{
   console.log(`Started on ${port}`);
 });
