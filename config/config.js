e8017e53fcad3f717b939744c358d2bcb89a4f4e
dinesh.kumawat <dinesh.kumawat@guidingstardigital.com>
2018-09-15 16:35:50 +0530

    config file changed

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 const env = process.env.NODE_ENV || 'development';
 const localdb = 'mongodb://localhost:27017/TodoApp';
+const devdb = 'mongodb://dinesh:todoapptestpassword1@ds257732.mlab.com:57732/todoapptest';
 if(env==='development'){
   process.env.PORT=3000;
-  process.env.MONGODB_URI = localdb;
+  process.env.MONGODB_URI = devdb;
 }
